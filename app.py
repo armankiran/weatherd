60c7590d76d22c5a54774026d537875c5bad7123
armankiran <armnkrn@gmail.com>
2022-04-20 18:18:35 +0100

    test

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def weather():
         if response.status_code == 200:
             return redirect(url_for("weatherd", postcode=postcode))
         elif response.status_code == 404:
-            return redirect("/")
+            return redirect("/weather")
     # get rain status
     rain = getall()[1]
     random_postcode = getpostcode()
@@ -48,12 +48,14 @@ def weatherd():
         weather = {'dt': '15:43:42', 'sunrise': '05:09:10', 'sunset': '18:58:03', 'temp': 9.1, 'feels_like': 10.48, 'pressure': 1028, 'humidity': 83, 'dew_point': 8.36, 'uvi': 1.56, 'clouds': 11, 'visibility': 10000, 'wind_speed': 6.24, 'wind_deg': 58, 'wind_gust': 8.01, 'id': '801', 'main': 'Clouds', 'description': 'few clouds'}
         rain = False if int(weather['id']) > 700 else True
         average_temp = getall()[0] 
-        city_rain_list = getall(weather['temp'])[2]
+        cities, wet_cities_size = getall(weather['temp'])[2], getall(weather['temp'])[3]
+
+
         drink = getdrink()
-        word = getword()
+        word = getword(rain)
         fact = getfact()
         pubs = getpubs(postcode)
-    return render_template("weatherd.html", location=location, weather=weather, average_temp=average_temp, postcode=postcode, rain=rain, city_rain_list=city_rain_list, drink=drink, word=word, fact=fact, pubs=pubs)
+    return render_template("weatherd.html", location=location, weather=weather, average_temp=average_temp, postcode=postcode, rain=rain, cities=cities, wet_cities_size=wet_cities_size, drink=drink, word=word, fact=fact, pubs=pubs)
 
 
 
